be35c9610408e4e8a2b1ff8d936230c3961588d8
Joel Alves Costa Filho <joelfilho93@gmail.com>
2013-11-23 15:57:03 -0200

    Implementando calculos do salario

--- ProjetoFinal/src/projetofinal/Funcionario.java
+++ ProjetoFinal/src/projetofinal/Funcionario.java
@@ -9,7 +9,9 @@ public class Funcionario {
    private String cpf; 
    private String rg;
    private double salario;
-
+   private double valeTransporte;
+   private double valeRefeicao;
+   
     public Funcionario(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
@@ -62,6 +64,75 @@ public class Funcionario {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-   
     
+     public double getValeRefeicao() {
+        return valeRefeicao;
+    }
+
+    public void setValeRefeicao(double valeRefeicao) {
+        this.valeRefeicao = valeRefeicao;
+    }
+
+    public double getValeTransporte() {
+        return valeTransporte;
+    }
+
+    public void setValeTransporte(double valeTransporte) {
+        this.valeTransporte = valeTransporte;
+    }
+   
+    public double calculaFerias(){
+        return this.getSalario()*0.11;
+    }
+    
+    public double calculaDecimoTerceiro(){
+        return this.getSalario()*0.08;
+    }
+    
+    public double calculaFGTS(){
+        return this.getSalario()*0.09;
+    }
+    
+    public double calculaMultaRecisao(){
+        return this.getSalario()*0.09;
+    }
+    
+    public double calculaINSS(){
+        return this.getSalario()*0.29;
+    }
+    
+    public double calculaEntidades(){
+        return this.getSalario()*0.02;
+    }
+    
+    public double calculaSalarioEducacao(){
+        return this.getSalario()*0.03;
+    }
+    
+    public double totalEncargos(){
+        return calculaFerias() + calculaDecimoTerceiro() + calculaFGTS() + calculaMultaRecisao() + calculaMultaRecisao() + calculaINSS() + calculaEntidades() + calculaSalarioEducacao(); 
+    }
+    
+    public double descontoFGTS(){
+        if (salario <= 375.00){
+            return 7.65;
+        }
+        else if(salario < 410.00){
+            return 8.65;
+        }
+        else if(salario <= 625.00){
+            return 9.00;
+        }
+        else{
+            return 11.00;
+        }
+    }
+    
+    public double salarioLiquido(){
+        return salario - descontoFGTS() + valeTransporte + valeRefeicao;
+    }
+    
+    public double custoTotalEmpresa(){
+        return salarioLiquido() + totalEncargos();
+    }
 }
