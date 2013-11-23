69ab67dbaae25241c10f17610f7d13be3cad7102
Joel Alves Costa Filho <joelfilho93@gmail.com>
2013-11-23 15:17:46 -0200

    Adicionando classe inicial do projeto

---
+++ ProjetoFinal/src/projetofinal/Funcionario.java
@@ -0,0 +1,67 @@
+
+package projetofinal;
+
+public class Funcionario {
+
+   private String nome;
+   private String sobrenome;
+   private String telefone;
+   private String cpf; 
+   private String rg;
+   private double salario;
+
+    public Funcionario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+   
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+   
+    
+}
