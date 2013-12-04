10c13918e57bf67689b02b8437c3c6c9b6349f11
Joel Alves Costa Filho <joelfilho93@gmail.com>
2013-12-04 14:50:37 -0200

    Criando controle de funcionário

--- ProjetoFinal/src/controller/ControleFuncionario.java
+++ ProjetoFinal/src/controller/ControleFuncionario.java
@@ -1,13 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
-/**
- *
- * @author SAMSUNG
- */
+import java.util.ArrayList;
+import models.Funcionario;
+
 public class ControleFuncionario {
     
+    private ArrayList<Funcionario> listaFuncionarios;
+    
+    public ControleFuncionario() {
+        this.listaFuncionarios = new ArrayList<Funcionario>();
+    }
+    
+    public void adicionar(Funcionario umFuncionario) {
+        listaFuncionarios.add(umFuncionario);
+    }
+    
+   public void remover(Funcionario umFuncionario) {
+        listaFuncionarios.remove(umFuncionario);
+    }
+    
+   public Funcionario pesquisar(String nome){
+       for(Funcionario umNome: listaFuncionarios){
+           if(umNome.getNome().equalsIgnoreCase(nome)) 
+               return umNome;
+       }
+       return null;
+   }
 }
